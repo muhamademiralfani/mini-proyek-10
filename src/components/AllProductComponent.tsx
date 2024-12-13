f21ec445de9c15c6c81236ab202c403c5d210185
muhamademiralfani <muhamademiralfani@gmail.com>
2024-12-13 16:34:10 +0700

    fix(twa): fix UI issues on TWA page

--- src/components/AllProductComponent.tsx
+++ src/components/AllProductComponent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts, setPage } from '../redux/async/productSlice';
